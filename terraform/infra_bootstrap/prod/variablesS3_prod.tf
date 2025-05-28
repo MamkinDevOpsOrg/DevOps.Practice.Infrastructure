f3b74300353bc274eab460cef410bcebb0ede80d
Artsiom-Kapset <Artsiom.Kapset@puregym.com>
2025-05-28 15:34:43 +0200

    reafctor: infra_bootstrap for prod updated

--- terraform/infra_bootstrap/prod/variablesS3_prod.tf
+++ terraform/infra_bootstrap/prod/variablesS3_prod.tf
@@ -1,11 +1,11 @@
 variable "region" {
   description = "Value of the region property for aws provider"
   type        = string
-  default     = "us-west-2"
+  default     = "us-west-1"
 }
 
 variable "s3_instance_name" {
   description = "Value of the name bucket in S3"
   type        = string
-  default     = "tfstate-storage-for-mamkindevops-prod-1"
+  default     = "tfstate-storage-for-mamkindevops-prod"
 }
